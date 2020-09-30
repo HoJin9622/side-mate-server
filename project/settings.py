74214d921dcf086e30da3d77abd0e8efda7bfee2
kimja01 <kimja7045@naver.com>
2020-09-30 10:14:30 +0900

    feat(static): Update static path

--- project/settings.py
+++ project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_FILE_STORAGE = 'project.storages.MediaStorage'
